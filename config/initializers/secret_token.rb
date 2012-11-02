3c0d2ed887d16f582f4ac964b16282df45e1cf99
Ruben Ascencio <galateaweb@gmail.com>
2012-11-02 09:07:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petshop::Application.config.secret_token = 'da7a548334eb59a5ab32c0771bf8680df17e0a2189157c9790d0d51c18c758ff616eab6a0cfc243a120b408c91b846b13a5600d64c801e75e522fba606d9a950'
